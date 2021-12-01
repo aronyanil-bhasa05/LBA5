e855741db65028620249733668d6ddee5271096e
aronyanil-bhasa05 <bhasa05@gmail.com>
2021-12-01 17:05:34 +0530

    Lab Based Assignment 5

---
+++ LBA5/LBA5/DiagSum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBA5
+{
+    internal class DiagSum
+    {
+    }
+}
---
+++ LBA5/LBA5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LBA5
+{
+    internal class ArrSum
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Array Size = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int [] arr = new int[n];
+            int s = 0;            
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"ENTER ELEMENT NUMBER {i + 1} = ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());              
+                s = s + arr[i];
+            }
+            Console.WriteLine("ARRAY ELEMENTS ARE ");
+            for (int i = 0; i < n; i++)
+                Console.Write($"{arr[i]}\t");
+            Console.WriteLine();
+            Console.Write($"SUM OF ALL ELEMENTS IS = {s}");
+            Console.ReadLine();
+        }
+       
+    }
+}
