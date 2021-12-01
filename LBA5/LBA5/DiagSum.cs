ed2f11286e9aff971c4ea44ead1da98e0de31700
aronyanil-bhasa05 <bhasa05@gmail.com>
2021-12-01 17:40:29 +0530

    Diag Sum

--- LBA5/LBA5/DiagSum.cs
+++ LBA5/LBA5/DiagSum.cs
@@ -8,5 +8,36 @@ namespace LBA5
 {
     internal class DiagSum
     {
+        static void Main()
+        {
+            Console.Write("Enter 2D Array Size (Square Matrix) = ");
+            int n=Convert.ToInt32(Console.ReadLine()); 
+            int[,] arr = new int[n, n];
+
+            for(int i=0;i<n;i++)
+            {
+               for(int j=0;j<n;j++)
+                {
+                    Console.Write($"Enter Element for pos ({i+1},{j+1}) = ");
+                    arr [i,j]=Convert.ToInt32(Console.ReadLine()); 
+                }
+            }
+            Console.WriteLine();
+            Console.Write("2D Array \n");
+            int s=0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{arr[i,j]}\t");
+                    if (i == j)
+                        s = s + arr[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.Write($"Sum of Diagonal is = {s}");
+            Console.ReadLine();
+        }
     }
 }
